6ee2388e8f5651ac9cfa7d2a09ff661dcc2f98c1
immonroe <immanuelmonroe@gmail.com>
2026-02-18 18:55:18 -0500

    init commit

---
+++ target/debug/curlrs.d
@@ -0,0 +1,1 @@
+/Users/manny/projects/curl-rs/target/debug/curlrs: /Users/manny/projects/curl-rs/src/main.rs
---
+++ target/debug/deps/curlrs-d3481d669cb10291.d
@@ -0,0 +1,5 @@
+/Users/manny/projects/curl-rs/target/debug/deps/curlrs-d3481d669cb10291.d: src/main.rs
+
+/Users/manny/projects/curl-rs/target/debug/deps/curlrs-d3481d669cb10291: src/main.rs
+
+src/main.rs:
